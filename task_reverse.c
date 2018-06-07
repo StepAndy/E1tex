d8bde1fd0948599150f898336eba077f9358a2fc
AndyStep <stepanoff.work@gmail.com>
2018-06-07 10:20:32 +0700

    solved multiple definiton +extern

--- task_reverse.c
+++ task_reverse.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-int reverse(int number)
-{
-	if(number>9999 || number <1000)
-		perror("Wrong number");
-	return number/1000 + (number/100 - (number/1000)*10)*10 + (number/10 - (number/100)*10)*100 + (number - (number/10)*10)*1000;
-}
+
+extern int reverse(int number);
 
 int main(int argc, char const *argv[])
 {
